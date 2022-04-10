9ced8c345e105614a726bd839f1ccc705863857b
Ganesh-Dev37 <ganesh0137@gmail.com>
2022-04-10 22:58:21 +0530

    FirstJenkinCommit

---
+++ com.jenkinProject/src/test/java/com/jenkinPractice/Demo.java
@@ -0,0 +1,17 @@
+package com.jenkinPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void j1()
+	{
+		System.out.println("j1 running");		
+	}
+	@Test
+	public void j2()
+	{
+	    System.out.println("j2 running");	
+	}
+
+}
---
+++ com.jenkinProject/src/test/java/com/jenkinPractice/Demo2.java
@@ -0,0 +1,16 @@
+package com.jenkinPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	@Test
+	public void g1()
+	{
+	System.out.println("g1 running");	
+	}
+	@Test
+    public void g2()
+    {
+    	System.out.println("g2 running");
+    }
+}
