25c12c794d45b7852bfa2b11ebf82e9a3b3f90a4
Ganesh-Dev37 <ganesh0137@gmail.com>
2022-04-15 18:59:32 +0530

    Happy New Year

---
+++ com.jenkinProject/src/test/java/com/jenkinPractice/HappyNewYear.java
@@ -0,0 +1,17 @@
+package com.jenkinPractice;
+
+import org.testng.annotations.Test;
+
+public class HappyNewYear {
+	@Test
+	public void h1()
+	{
+		System.out.println("Happy New Year 1");
+	}
+	@Test
+	public void h2()
+	{
+		System.out.println("Happy New Year 2");
+	}
+
+}
